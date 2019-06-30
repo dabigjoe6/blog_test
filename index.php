fe181e6c183df15c6ab8edda697b66d992876607
Joseph <dabigjoe6@gmail.com>
2019-06-30 22:33:11 +0100

    Visuals for the homepage

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="https://st<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="container pt-5">
+            <div class="d-flex justify-content-between">
+                <h1 class="mb-5">Blog posts</h1>
+                <button class="btn btn-primary btn-sm" style="height: 40px">Create Blog post</button>
+            </div>
+            <div class="d-flex flex-column align-items-center">
+                <div class="col-6 my-3">
+                    <h4 class="font-weight-bold mb-0">Title</h4>
+                    <p class="font-weight-normal">Date Created</p>
+                    <p class="text-wrap mb-0">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus libero ratione, iure architecto est esse eum veritatis consectetur error ut? Aspernatur voluptatem, porro ipsam culpa quod illum id architecto officia?</p>
+                    <button type="button" class="btn btn-link mt-3" style="position: absolute; right: 0px">Edit</button>
+                </div>
+            </div>
+            
+
+        </div>
+    </body>
+</html>
